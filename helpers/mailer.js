b50a34643f199d96fb3b6b50d1e7322785326714
midham14 <mohammadidham14@gmail.com>
2020-09-24 23:27:37 +0700

    nodemailer & proteksi

--- helpers/mailer.js
+++ helpers/mailer.js
@@ -1,59 +1,16 @@
-const express = require('express')
-const sendToMeRouter = express.Router()
 const nodemailer = require('nodemailer')
 
-console.log("from sendToMe")
-
-module.exports = sendToMeRouter
-app.use('/sendtome', require('./routes/sendToMe'))
-
-const transport = {
-    //all of the configuration for making a site send an email.
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
-    auth: {
-        user: process.env.THE_EMAIL,
-        pass: process.env.THE_PASSWORD
+const transporter=nodemailer.createTransport({
+    service:'gmail',
+    auth:{
+        user:'idhamdummy3@gmail.com',
+        pass:'fwfmycwvxtpbgrno'
+    },
+    tls:{
+        rejectUnauthorized:false
     }
-};
 
-const transporter = nodemailer.createTransport(transport);
-transporter.verify((error, success) => {
-    if (error) {
-        //if error happened code ends here
-        console.error(error)
-    } else {
-        //this means success
-        console.log('users ready to mail myself')
-    }
-});
-
-sendToMeRouter.post('/', (req, res, next) => {
-    //make mailable object
-    const mail = {
-        from: process.env.THE_EMAIL,
-        to: 'moerhamsa@gmail.com',
-        subject: req.body.subject,
-        text: `
-      from:
-      ${req.body.name} 
-      contact: ${req.body.email}
-      message: 
-
-      ${req.body.text}`
-    }
-    // error handling goes here. 
-});
-
-transporter.sendMail(mail, (err, data) => {
-    if (err) {
-        res.json({
-            status: 'fail'
-        })
-    } else {
-        res.json({
-            status: 'success'
-        })
-    }
 })
+
+
+module.exports=transporter
